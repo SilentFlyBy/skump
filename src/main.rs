24d0fa2d7caec775b19d03e8e402e1106beee97d
Erik Moldtmann <erik@moldtmann.de>
2020-09-18 21:46:54 +0200

    another

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use clap::{App, Arg, ArgMatches};
+mod list;
 
 fn main() {
-    let matches = App::new("My Super Program")
-        .version("1.0")
-        .author("Kevin K. <kbknapp@gmail.com>")
-        .about("Does awesome things")
+    let mut app = App::new("Skump - Skype dumper")
+        .version("0.1")
+        .author("Erik M. <erik@moldtmann.de>")
+        .about("Tool for exporting skype chats to various sources")
         .arg(
             Arg::with_name("output format")
                 .short('o')
@@ -19,15 +20,22 @@ fn main() {
                 .about("Sets the input file to use")
                 .index(1),
         )
-        .subcommand(App::new("ls").about("lists chats").alias("list"))
-        .get_matches();
+        .subcommand(App::new("ls").about("lists chats").alias("list"));
+
+    let matches = app.get_matches_mut();
 
     match matches.subcommand() {
-        ("ls", Some(_sub_m)) => list(_sub_m),
-        _ => {}
-    }
+        ("ls", Some(_sub_m)) => list_command(_sub_m).unwrap(),
+        _ => main_command(&matches).unwrap(),
+    };
 }
 
-fn list(args: &ArgMatches) {
-    println!("test")
+fn list_command(_args: &ArgMatches) -> Result<(), ()> {
+    println!("list");
+    Ok(())
+}
+
+fn main_command(_args: &ArgMatches) -> Result<(), ()> {
+    println!("main");
+    Ok(())
 }
